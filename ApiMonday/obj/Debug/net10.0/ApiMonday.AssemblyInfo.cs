06deb8dc516ef5b75449dfb10b5bcec884c747e4
Riana ghd <ri.ghadamzadeh@gmail.com>
2026-03-24 10:41:37 +0100

    files

--- ApiMonday/obj/Debug/net10.0/ApiMonday.AssemblyInfo.cs
+++ ApiMonday/obj/Debug/net10.0/ApiMonday.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiMonday")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e220f91124d48dd78162eb310b86fceb419e2e8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f754c6f1d2ebd652e20dd65a35775acfdcacd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiMonday")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiMonday")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
